e9fdabfba0c9393cd8bb751f8953bf66ac26f8d9
henryortiz23 <henryortiz_m@hotmail.com>
2023-11-20 19:47:05 -0600

    Subida
    Trabajo: Henry Adalid Ortiz Meza (201310010533)

---
+++ AudioRecorder/Models/Audios.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace AudioRecorder.Models
+{
+    public class Audios
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        [MaxLength(20)]
+        public string fecha { get; set; }
+
+        public byte[] audio { get; set; }
+
+    }
+}
